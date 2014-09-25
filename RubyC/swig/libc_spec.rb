2c225e6dc47691b7cbfb80af74c18c835c4a0605
Arnd R. Strube <a.strube@wind-soft.de>
2014-09-25 14:33:31 +0200

    Add libc specs.

---
+++ RubyC/swig/libc_spec.rb
@@ -0,0 +1,15 @@
+require './libc'
+
+describe "Using Libc" do
+  context "memory" do
+    it "can be allocated"
+    it "can be freed"
+  end
+  context "a file" do
+    it "can be opened for reading" 
+    it "can be opened for writing"
+    it "can be read"
+    it "can be closed"
+    it "can be written"
+  end
+end
